3d85cc9cabdaaeab3f10d33ed4f77b45e9d7c197
alex@ARWin <alex@ARWin.com>
2022-03-15 21:48:27 +0800

    c03-quiz_cmdline passed

--- c03-quiz_cmdline/src/main.rs
+++ c03-quiz_cmdline/src/main.rs
@@ -1,15 +1,22 @@
 use std::fs;
 
+
+//To test: cargo run -- https://www.rust-lang.org rust.md
 fn main() {
-    let url = "http://www.rust-lang.org/";
-    let output = "rust.md";
+    let args = std::env::args().collect::<Vec<String>>();
 
-    println!("Fetching url: {}", url);
-    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    if let [_path, url, output] = args.as_slice() {
+        println!(">> Processing: url={}, output={}", url, output);
+        println!("Fetching url: {}", url);
+        let body = reqwest::blocking::get(url).unwrap().text().unwrap();
 
-    println!("Converting html to markdown ...");
-    let md = html2md::parse_html(&body);
+        println!("Converting html to markdown ...");
+        let md = html2md::parse_html(&body);
 
-    fs::write(output,md.as_bytes()).unwrap();
-    println!("Converted markdown has been saved in {}.", output);
+        fs::write(output, md.as_bytes()).unwrap();
+        println!("Converted markdown has been saved in {}.", output);
+    } else {
+        eprintln!("!! Please input args in format of 'url output'");
+    }
+
 }
